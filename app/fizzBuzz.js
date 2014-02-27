69dbc79622123e0af95e63c2d7375d9a9bb83f0c
Jose Ángel Barroso <joseangel.barroso@gmail.com>
2014-02-27 22:03:16 +0100

    Green

--- app/fizzBuzz.js
+++ app/fizzBuzz.js
@@ -3,10 +3,17 @@ var fizzBuzz = (function() {
         factor: 3,
         result: 'Fizz'
     };
+    var buzz = {
+        factor: 5,
+        result: 'Buzz'
+    };
     return {
         say: function(number) {
             if (number % fizz.factor === 0) {
                 return fizz.result;
+            }
+            if (number % buzz.factor === 0) {
+                return buzz.result;
             }
             return number;
         }
