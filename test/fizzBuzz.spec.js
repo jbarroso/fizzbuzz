f3f0870897a223f3fc5b11ad0d65ef8b44001180
Jose Ángel Barroso <joseangel.barroso@gmail.com>
2014-02-27 21:45:21 +0100

    Should say number when it is 1

--- test/fizzBuzz.spec.js
+++ test/fizzBuzz.spec.js
@@ -2,4 +2,7 @@ describe('fizzBuzz', function() {
     it('should be an object', function() {
         expect(fizzBuzz).to.be.an('object');
     });
+    it('should say number when it is 1', function() {
+        expect(fizzBuzz.say(1)).to.be.equals(1);
+    });
 });
